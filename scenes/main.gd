d88e8798ccef3ff18c54ceb3f2148e92e6a0c08d
Moxvallix <moxvallix@gmail.com>
2022-06-19 22:30:49 +0930

    feat(dialog): add kindling dialog, add more assets toFS

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,6 +2,5 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Stats.add_sincerity("3")
-	var new_dialog = Dialogic.start("test")
+	var new_dialog = Dialogic.start("kindling")
 	add_child(new_dialog)
