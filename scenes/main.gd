a79637adddc9a3bc3092b1b4da5389f8e419c73d
Moxvallix <moxvallix@gmail.com>
2022-06-19 20:34:16 +0930

    feat(character): add poohbear angry

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,5 +3,5 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Stats.add_sincerity("3")
-	var new_dialog = Dialogic.start()
+	var new_dialog = Dialogic.start("test")
 	add_child(new_dialog)
